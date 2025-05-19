a656ab65fcdfd9a9865aeb7681ac61811fb8d0de
Alec Cotton <aleccotton@cmail.carleton.ca>
2025-05-19 17:39:40 -0400

    setup

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, Extension
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 import os
 
+# Use CONDA_PREFIX if set, otherwise default to /usr
+eigen_path = os.path.join(os.getenv("CONDA_PREFIX", "/usr"), "include/eigen3")
+
 ext_modules = [
     Pybind11Extension(
         "dh_kinematics",
         ["src/dh_kinematics.cpp", "src/pybind_wrapper.cpp"],
-        include_dirs=["include", os.path.join(os.getenv("CONDA_PREFIX"), "include/eigen3")],
+        include_dirs=["include", eigen_path],
         extra_compile_args=["-O3"],
     ),
 ]
